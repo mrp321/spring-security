fc9ab0479a95667782855047b7e7c42476605af1
qchen <y7654f@gmail.com>
2018-04-15 16:07:32 +0800

    1. 用户密码加密

--- security-browser/src/main/java/cn/sitedev/browser/userdetail/MyUserDetailsService.java
+++ security-browser/src/main/java/cn/sitedev/browser/userdetail/MyUserDetailsService.java
@@ -2,11 +2,14 @@ package cn.sitedev.browser.userdetail;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,26 +21,29 @@ import org.springframework.stereotype.Component;
 public class MyUserDetailsService implements UserDetailsService {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    /**
+     * 密码编码
+     *
+     * @return
+     */
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         logger.info("登陆用户名" + username);
+        // 密码加密
+        String password = passwordEncoder().encode("123456");
+        logger.info("登陆密码[加密]" + password);
         // 1. 根据用户名查找用户信息
         // User(String username, String password,  Collection<? extends GrantedAuthority> authorities)
         // username : 用户名
         // password : 密码
         // authorities : 用户权限集合
         // AuthorityUtils.commaSeparatedStringToAuthorityList(String authorityString): 将以逗号分隔的权限字符串转为权限集合
-        // return new User(username, "123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        return new User(username, password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
 
-        // 2. 根据查找到的用户信息判断用户是否被冻结
-        // User(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities)
-        // username : 用户名
-        // password : 密码
-        // enabled : 是否被禁用
-        // accountNonExpired : 账号是否过期
-        // credentialsNonExpired : 密码是否过期
-        // accountNonLocked : 账号是否锁定
-        // authorities : 用户权限集合
-        return new User(username, "123456", true, true, true, false, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
