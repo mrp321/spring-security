d621661084cd12f8b770b71e884e179d5193fcc8
qchen <y7654f@gmail.com>
2018-04-15 14:46:07 +0800

    1. 自定义用户认证逻辑(根据查找到的用户信息判断用户是否被冻结)

--- security-browser/src/main/java/cn/sitedev/browser/userdetail/MyUserDetailsService.java
+++ security-browser/src/main/java/cn/sitedev/browser/userdetail/MyUserDetailsService.java
@@ -21,13 +21,23 @@ public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         logger.info("登陆用户名" + username);
-        // 根据用户名查找用户信息
+        // 1. 根据用户名查找用户信息
         // User(String username, String password,  Collection<? extends GrantedAuthority> authorities)
         // username : 用户名
         // password : 密码
         // authorities : 用户权限集合
         // AuthorityUtils.commaSeparatedStringToAuthorityList(String authorityString): 将以逗号分隔的权限字符串转为权限集合
-        return new User(username, "123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        // return new User(username, "123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
 
+        // 2. 根据查找到的用户信息判断用户是否被冻结
+        // User(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities)
+        // username : 用户名
+        // password : 密码
+        // enabled : 是否被禁用
+        // accountNonExpired : 账号是否过期
+        // credentialsNonExpired : 密码是否过期
+        // accountNonLocked : 账号是否锁定
+        // authorities : 用户权限集合
+        return new User(username, "123456", true, true, true, false, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
